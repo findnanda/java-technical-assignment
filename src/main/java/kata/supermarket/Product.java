f427512bf056b625594666cef47387a68022f1ec
Nanda <lan9le@bosch.com>
2022-04-21 14:39:36 +0100

    Add name field to the product since two different products can have same pricing

--- src/main/java/kata/supermarket/Product.java
+++ src/main/java/kata/supermarket/Product.java
@@ -4,17 +4,23 @@ import java.math.BigDecimal;
 
 public class Product {
 
+    private final String name;
     private final BigDecimal pricePerUnit;
 
-    public Product(final BigDecimal pricePerUnit) {
+    public Product(final BigDecimal pricePerUnit, final String name) {
         this.pricePerUnit = pricePerUnit;
+        this.name = name;
     }
 
     BigDecimal pricePerUnit() {
         return pricePerUnit;
     }
 
-    public Item oneOf() {
-        return new ItemByUnit(this);
+    public Item ofQuantity(int quantity) {
+        return new ItemByUnit(this, new BigDecimal(quantity));
+    }
+
+    String name(){
+       return this.name;
     }
 }
